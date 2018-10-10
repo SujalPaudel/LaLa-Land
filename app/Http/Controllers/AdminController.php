084f342cb15924aa57b70ea325d7c59f15cc805f
Velar <Sujal Paudel>
2018-10-10 09:48:50 +0545

    first commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Auth;
+use Session;
+
+class AdminController extends Controller
+{
+    public function login(Request $request){
+      if($request->isMethod('post')){
+        $data = $request->input();
+        if(Auth::attempt(['email'=>$data['email'], 'password'=>$data['password'], 'admin'=>'1'])){ // to check the credentials of admin
+          return redirect('/admin/dashboard');
+
+        }else{
+          return redirect('/admin')->with('flash_message_error', 'Invalid Username or Password');
+        }
+      }
+      return view('admin.admin_login'); // inside the admin folder in view
+    }
+
+    public function dashboard(){
+      return view('admin.dashboard');
+    }
+
+    public function logout(){
+      Session::flush();
+      return redirect('/admin')->with('flash_message_success', 'Logged Out Successfully');
+
+    }
+}
