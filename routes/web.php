60a129a35ea0dfea60697e1d9b3913c314e46e50
Velar <thesujal17@gmail.com>
2018-10-13 09:49:20 +0545

    Adding product user interface

--- routes/web.php
+++ routes/web.php
@@ -29,10 +29,17 @@ Route::group(['middleware' => ['auth']], function(){
   Route::get('/admin/check-pwd', 'AdminController@chkPassword');
   Route::match(['get', 'post'], '/admin/update-pwd', 'AdminController@updatePassword');
 
+  // Categories Routes (Admin)
+
   Route::match(['get', 'post'], '/admin/add-category', 'CategoryController@addCategory'); // both the get and the post method 
   Route::match(['get', 'post'], '/admin/edit-category/{id}', 'CategoryController@editCategory');
   Route::match(['get', 'post'], '/admin/delete-category/{id}', 'CategoryController@deleteCategory');
   Route::get('/admin/view-categories', 'CategoryController@viewCategories');
+
+  // Products Routes
+
+  Route::match(['get', 'post'], '/admin/add-product', 'ProductsController@addProduct');
+
 });
 
 Route::get('/home', 'HomeController@index')->name('home');
