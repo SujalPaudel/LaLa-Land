70c1bb803958ee7ae55bdf3784a9d7710f541db1
Velar <thesujal17@gmail.com>
2018-10-18 15:00:27 +0545

    Products shown in homepage

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Products;
 
 class IndexController extends Controller
 {
     public function index(){
-      return view('index');
+
+      // In ascending order
+      // $productsAll = Products::get();
+
+      // In descending order
+      $productsAll = Products::orderBy('id', 'DESC')->get();
+
+      // In random order
+      // $productsAll = Products::inRandomOrder()->get();
+      
+      return view('index')->with(compact('productsAll'));
     }
 }
