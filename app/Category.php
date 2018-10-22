9622e9f4bb0869085fb3764e49336a51369bdc85
Velar <thesujal17@gmail.com>
2018-10-22 18:12:47 +0545

    Used relational approach to bring data from backend to FE

--- app/Category.php
+++ app/Category.php
@@ -4,7 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Category extends Model
 {
-    
+    public function subcategories(){
+      return $this->hasMany('App\Category', 'parent_id');
+    }
 }
--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -21,33 +21,36 @@ class IndexController extends Controller
 
       $categories_menu = "";
 
+      $categories= Category::with('subcategories')->where(['parent_id'=>0])->get();
+      // $categories = json_decode(json_encode($categories));
+      // echo "<pre>";print_r($categories);die;
+      // foreach($categories as $cat){
 
-      $categories= Category::where(['parent_id'=>0])->get();
-      // $categoriesAll = json_decode(json_encode($categoriesAll));
-      foreach($categories as $cat){
-
-        $categories_menu .= "<div class = 'panel-heading'>
-                              <h4 class = 'panel-title'>
-                                <a data-toggle='collapse' data-parent='#accordian' href='#".$cat->id."'>
-                                  <span class='badge pull-right'><i class='fa fa-plus'></i></span>
-                                  ".$cat->name."
-                                </a>
-                              </h4>
-                            </div>
-                            <div id='".$cat->id."' class='panel-collapse collapse'>
-                              <div class='panel-body'>
-                                <ul>";
-                                $sub_categories = Category::where(['parent_id'=>$cat->id])->get();
-                                foreach($sub_categories as $subcat){
-                                  $categories_menu .= "<li><a href='".$subcat->url."'>".$subcat->name."</a></li>";
-                                }
-                                $categories_menu .= "</ul>
-                              </div>
-                            </div>";                     
+      //   $categories_menu .= "<div class = 'panel-heading'>
+      //                         <h4 class = 'panel-title'>
+      //                           <a data-toggle='collapse' data-parent='#accordian' href='#".$cat->id."'>
+      //                             <span class='badge pull-right'><i class='fa fa-plus'></i></span>
+      //                             ".$cat->name."
+      //                           </a>
+      //                         </h4>
+      //                       </div>
+      //                       <div id='".$cat->id."' class='panel-collapse collapse'>
+      //                         <div class='panel-body'>
+      //                           <ul>";
+      //                           $sub_categories = Category::where(['parent_id'=>$cat->id])->get();
+      //                           foreach($sub_categories as $subcat){
+      //                             $categories_menu .= "<li><a href='".$subcat->url."'>".$subcat->name."</a></li>";
+      //                           }
+      //                           $categories_menu .= "</ul>
+      //                         </div>
+      //                       </div>";                     
 
 
-        }
-        return view('index')->with(compact('productsAll', 'categories_menu'));
-      }
+      //   }
+      //   return view('index')->with(compact('productsAll', 'categories_menu'));
+      // }
+      return view('index')->with(compact('productsAll', 'categories_menu', 'categories'));
+
 
     }
+  }
